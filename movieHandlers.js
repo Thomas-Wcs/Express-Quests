a6b99ab672d49cb88793937dcce29ec44e5d105d
Thomas <119404291+Thomas-Wcs@users.noreply.github.com>
2023-03-23 16:23:41 +0100

    postUsersEnd

--- movieHandlers.js
+++ movieHandlers.js
@@ -1,5 +1,27 @@
 const database = require('./database');
 
+const updateMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, director, year, color, duration } = req.body;
+
+  database
+    .query(
+      'update movies set title = ?, director = ?, year = ?, color = ?, duration = ? where id = ?',
+      [title, director, year, color, duration, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send('Film Not Found');
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error editing the movie');
+    });
+};
+
 const postMovie = (req, res) => {
   const { title, director, year, color, duration } = req.body;
 
@@ -51,4 +73,5 @@ module.exports = {
   getMovies,
   getMovieById,
   postMovie,
+  updateMovie,
 };
--- usersHandlers.js
+++ usersHandlers.js
@@ -1,5 +1,27 @@
 const database = require('./database');
 
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      'update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?',
+      [firstname, lastname, email, city, language, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send('User Not Found');
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error editing the user');
+    });
+};
+
 const postUsers = (req, res) => {
   const { firstname, lastname, email, city, language } = req.body;
 
@@ -51,4 +73,5 @@ module.exports = {
   getUsers,
   getUsersById,
   postUsers,
+  updateUser,
 };
