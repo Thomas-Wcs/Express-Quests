e8398954d7089fe456b432cbbf4df6822f2c4f80
Thomas <119404291+Thomas-Wcs@users.noreply.github.com>
2023-03-23 16:38:59 +0100

    deleteUser

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.put('/api/movies/:id', movieHandlers.updateMovie);
 app.put('/api/users/:id', userHandlers.updateUser);
 
 app.delete('/api/movies/:id', movieHandlers.deleteMovie);
+app.delete('/api/users/:id', userHandlers.deleteUsers);
 
 app.listen(port, (err) => {
   if (err) {
